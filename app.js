5e34dfadd97321d7186d36c08cbbf747ea3f4b59
Marija <mmilicevic@institutmvm.cat>
2026-02-26 09:25:48 +0100

    Added LlistaFinal

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+const consumicions = [
+  { nom: 'Cervesa', preu: 5 },
+  { nom: 'Copa Premium', preu: 15 },
+  { nom: 'Aigua', preu: 3 },
+  { nom: 'Shot de Tequila', preu: 8 },
+  { nom: 'Ampolla reservat', preu: 200 }
+];
+
+const llistaFinal = consumicions 
+.filter(n => n.preu > 50)
+.map(n => n.nom ? `${n.nom} CARO 💸` : `${n.nom} ACCEPTABLE ✅`);
+
+console.log(llistaFinal);
